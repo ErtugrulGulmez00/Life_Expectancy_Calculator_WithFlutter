b5686d205761ac5f7bd5ab6a7c384fa44e92e142
Ertugrul Gulmez <ertu246@gmail.com>
2023-08-13 20:03:56 +0300

    created homescreen and added button

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch:
+          Colors.orange,
+            accentColor: Colors.pink,
+
+
+          )
+      ),
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: const Text(
+          'YAŞAM BEKLENTİSİ',
+          style: TextStyle(color: Colors.black54),
+        ),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('Form Alanı'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
